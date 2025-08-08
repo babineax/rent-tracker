7c000b9df91b8e6db6768774405afe5c8e540c76
babineax <ivysuite8@gmail.com>
2025-08-08 10:53:13 +0300

    sprint1

--- rent-management/src/pages/LandingPage.jsx
+++ rent-management/src/pages/LandingPage.jsx
@@ -6,8 +6,8 @@ function LandingPage() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
-      <img src={logo} alt="Logo" className="w-32 h-32 mb-6" />
-      <h1 className="text-3xl font-bold mb-4 text-center">Welcome to RentEase</h1>
+      <img src={logo} alt="Logo" className="w-72 h-72 mb-6" />
+      <h1 className="text-3xl font-bold mb-4 text-center">Welcome to simplified renting with rentease.</h1>
       <p className="mb-8 text-gray-600 text-center">Track and manage your properties with ease.</p>
       <div className="flex space-x-4">
         <button
